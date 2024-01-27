95b5dbc9a2ae7dc7855ec9b44f307be4813a137c
Ekaterina Shiverskikh <katerina_kononenko_94@mail.ru>
2024-01-27 17:28:50 +0700

    added homework5

---
+++ homework5/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<ProductDetails />
+</template>
+
+<script>
+import ProductDetails from './components/ProductDetails.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    ProductDetails
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
